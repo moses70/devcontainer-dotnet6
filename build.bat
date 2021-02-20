4e3c8988397749e774f34cfeedac2033904d0c07
Reinhard Moosauer <office@moosauer.de>
2021-02-20 18:04:37 +0100

    Added tagging

--- build.bat
+++ build.bat
@@ -5,3 +5,6 @@ set _tag=preview1
 docker build .devcontainer -t moses70/dotnet6:%_tag% --build-arg VARIANT=6.0 --build-arg INSTALL_NODE=false --build-arg INSTALL_ZSH=false --build-arg INSTALL_AZURE_CLI=false --build-arg UPGRADE_PACKAGES=true
 
 docker push moses70/dotnet6:%_tag%
+
+docker tag moses70/dotnet6:%_tag% moses70/dotnet6:latest
+docker push moses70/dotnet6:latest
