1e2f8d7d8fec1223c8c0f3cffba3f9d82b6f2054
Reinhard Moosauer <office@moosauer.de>
2021-03-21 15:13:28 +0100

    Created NET6 preview2

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set _tag=preview1
+set _tag=preview2
 
 docker build .devcontainer -t moses70/dotnet6:%_tag% --build-arg VARIANT=6.0 --build-arg INSTALL_NODE=false --build-arg INSTALL_ZSH=false --build-arg INSTALL_AZURE_CLI=false --build-arg UPGRADE_PACKAGES=true
 
