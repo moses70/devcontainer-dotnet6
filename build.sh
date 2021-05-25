3714f98c3662eea6eb8c27476ec897148a816462
Reinhard Moosauer <office@moosauer.de>
2021-05-25 18:26:52 +0000

    Updated for preview 4

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_tag=preview3
+_tag=preview4
 
 docker build .devcontainer -t moses70/dotnet6:$_tag --build-arg VARIANT=6.0 --build-arg INSTALL_NODE=false --build-arg INSTALL_ZSH=false --build-arg INSTALL_AZURE_CLI=false --build-arg UPGRADE_PACKAGES=true
 
